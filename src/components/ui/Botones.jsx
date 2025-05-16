ac6e2b3945204abf29ba2f562e31083dc430b4b2
Carlos <pokejagger@gmail.com>
2025-05-16 14:32:36 -0600

    boton

---
+++ src/components/ui/Botones.jsx
@@ -0,0 +1,8 @@
+
+export const Botones = () => {
+  return (
+    <button className="bg-blue-500 text-white hover:bg-white hover:text-blue-500 hover:border-blue-500 hover:border-1 font-bold py-2 px-4 rounded transition-colors">
+        Descargar CV
+    </button>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './styles.css'
+import { Home } from './pages/Home'
+import { Botones } from './components/ui/Botones'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Botones/>
   </StrictMode>,
 )
